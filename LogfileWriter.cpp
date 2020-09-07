b522381970861a5582ce30efc5ba5fb21aea2992
necotec <64785948+necotec@users.noreply.github.com>
2020-09-07 10:39:16 +0200

    Initial commit of version 1.0

---
+++ LogfileWriter.cpp
@@ -0,0 +1,199 @@
+/*
+*   A little class to take a log.
+*   Author: necotec(Oliver ALFF)
+*/
+
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include "LogfileWriter.h"
+
+#include <algorithm>
+#include <string>
+
+//using namespace std;
+
+//extern LogfileWriter log;
+
+LogfileWriter::LogfileWriter()
+{
+
+}
+
+LogfileWriter::~LogfileWriter()
+{
+
+}
+
+void LogfileWriter::Test()
+{
+    std::cout << testString << std::endl;
+}
+
+bool LogfileWriter::SetLogfilePath(std::string path)
+{
+    bool returnval = false;
+
+    try{
+        LogfilePath = path;
+        returnval = true;
+    }
+    catch(std::string& ex)
+    {
+        std::cout << "Error by set the path of the logfile: " << ex.c_str() << std::endl;
+    }
+    return returnval;
+}
+
+bool LogfileWriter::SetLogfileName(std::string name)
+{
+    bool returnval = false;
+
+    try{
+        LogfileName = name;
+
+
+
+        returnval = true;
+    }
+    catch(std::string& ex)
+    {
+        std::cout << "Error by set the name of the logfile: " << ex.c_str() << std::endl;
+    }
+    return returnval;
+}
+
+std::string LogfileWriter::GetLogfilePath()
+{
+    return LogfilePath;
+}
+
+std::string LogfileWriter::GetLogfileName()
+{
+    std::string texpText = "";
+
+    //std::cout << "First char of the logfile name: " << LogfileName[0] << std::endl;
+    //std::cout << "Second char of the logfile name: " << LogfileName[1] << std::endl;
+
+    return LogfileName;
+}
+
+bool LogfileWriter::DoLog(std::string logtext)
+{
+    bool returnval = true;
+    std::string tempLogText = "";
+
+    try{
+        // declare time variables
+        time_t currentTime = time(NULL);
+
+        std::ofstream logfile;
+        logfile.open(GetLogfilePath() + "\\" + GetLogfileName(), std::ios_base::app);
+
+        if(logfile.is_open())
+        {
+            tempLogText = ctime(&currentTime);
+
+            // delete \n from timestring
+            tempLogText.erase(std::remove(tempLogText.begin(), tempLogText.end(), '\n'), tempLogText.end());
+
+            tempLogText += " - ";
+            tempLogText += logtext;
+            tempLogText += "\n";
+
+            std::cout << std::endl << "The logtext: " + tempLogText << std::endl << std::endl;
+
+            std::cout << "Opened file from " << GetLogfilePath() + "\\" + GetLogfileName() << " successfully." << std::endl;;
+
+            logfile << tempLogText;
+
+            logfile.close();
+        }
+        returnval = true;
+    }
+    catch(std::string& ex)
+    {
+        std::cout << "Error by writing to the logfile: " << ex.c_str() << std::endl;
+    }
+
+    return returnval;
+}
+
+bool LogfileWriter::DoLog(std::string logtext, LogLevel logLevel)
+{
+    bool returnval = true;
+    std::string tempLogText = "";
+
+    try{
+        // declare time variables
+        time_t currentTime = time(NULL);
+
+        std::ofstream logfile;
+        logfile.open(GetLogfilePath() + "\\" + GetLogfileName(), std::ios_base::app);
+
+        if(logfile.is_open())
+        {
+            tempLogText = ctime(&currentTime);  // set current time
+
+            // delete \n from timestring
+            tempLogText.erase(std::remove(tempLogText.begin(), tempLogText.end(), '\n'), tempLogText.end());
+
+            tempLogText += " - ";
+            tempLogText += GetLogLevelString(logLevel);
+            tempLogText += " - ";
+            tempLogText += logtext;
+            tempLogText += "\n";
+
+            std::cout << std::endl << "The logtext: " + tempLogText << std::endl << std::endl;
+
+            //std::cout << "Opened file from " << getLogfilePath() + "\\" + getLogfileName() << " successfully." << std::endl;;
+
+            logfile << tempLogText;
+
+            logfile.close();
+        }
+        returnval = true;
+    }
+    catch(std::string& ex)
+    {
+        std::cout << "Error by writing to the logfile: " << ex.c_str() << std::endl;
+    }
+
+    return returnval;
+}
+
+std::string LogfileWriter::BoolToString(bool b)
+{
+    return b ? "true" : "false";
+}
+
+std::string LogfileWriter::GetLogLevelString(LogLevel logLevel)
+{
+    std::string logLevelString;
+
+    switch(logLevel)
+    {
+        case 1:
+            logLevelString = "Debug";
+            //std::cout << "Added label Debug" << std::endl;
+            break;
+        case 2:
+            logLevelString = "Info";
+            //std::cout << "Added label Info" << std::endl;
+            break;
+        case 3:
+            logLevelString = "Warning";
+            //std::cout << "Added label Warning" << std::endl;
+            break;
+        case 4:
+            logLevelString = "Error";
+            //std::cout << "Added label Error" << std::endl;
+            break;
+        case 5:
+            logLevelString = "Fatal";
+            //std::cout << "Added label Fatal" << std::endl;
+            break;
+    }
+
+    return logLevelString;
+}
---
+++ LogfileWriter.h
@@ -0,0 +1,45 @@
+/*
+*   A little class to take a log.
+*   Author: necotec(Oliver ALFF)
+*/
+
+#ifndef LOGFILEWRITER_H_INCLUDED
+#define LOGFILEWRITER_H_INCLUDED
+
+#include <iostream>
+
+enum LogLevel
+{
+    Debug = 1,
+    Info = 2,
+    Warning = 3,
+    Error = 4,
+    Fatal = 5
+};
+
+class LogfileWriter{
+protected:
+    std::string testString = "teststring protected";
+
+private:
+    std::string LogfilePath = "";
+    std::string LogfileName = "";
+
+public:
+    LogfileWriter();
+    ~LogfileWriter();
+    void Test();
+    bool SetLogfilePath(std::string path);
+    bool SetLogfileName(std::string name);
+    std::string GetLogfilePath();
+    std::string GetLogfileName();
+    bool DoLog(std::string logtext);
+    bool DoLog(std::string logtext, LogLevel logLevel);
+    std::string BoolToString(bool b);
+
+private:
+    std::string GetLogLevelString(LogLevel logLevel);
+};
+
+//extern class LogfileWriter;
+#endif // LOGFILEWRITER_H_INCLUDED
